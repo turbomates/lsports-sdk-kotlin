390c98f3c25a4e40f6791d6c9327eaa40e65deab
Dima Korotin <dskorotin@gmail.com>
2022-01-24 10:58:38 +0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,7 +123,6 @@ publishing {
             }
         }
     }
-
     repositories {
         maven {
             val releasesUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")

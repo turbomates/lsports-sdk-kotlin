1c67711236317ff9db05e0410ea4eb112d2cf330
vesurbag <vesurbag@gmail.com>
2022-02-08 10:17:31 +0300

    Direct call handler methods

--- src/main/kotlin/com/turbomates/kotlin/lsports/sdk/listener/Handler.kt
+++ src/main/kotlin/com/turbomates/kotlin/lsports/sdk/listener/Handler.kt
@@ -1,6 +1,5 @@
 package com.turbomates.kotlin.lsports.sdk.listener
 
-import com.turbomates.kotlin.lsports.sdk.model.Message
 import com.turbomates.kotlin.lsports.sdk.model.message.FixtureUpdateMessage
 import com.turbomates.kotlin.lsports.sdk.model.message.HeartbeatMessage
 import com.turbomates.kotlin.lsports.sdk.model.message.KeepAliveMessage
@@ -9,7 +8,6 @@ import com.turbomates.kotlin.lsports.sdk.model.message.MarketUpdateMessage
 import com.turbomates.kotlin.lsports.sdk.model.message.SettlementMessage
 
 interface Handler {
-    fun handle(message: Message)
     fun handle(message: FixtureUpdateMessage)
     fun handle(message: LivescoreUpdateMessage)
     fun handle(message: MarketUpdateMessage)
